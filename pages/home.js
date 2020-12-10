21614a39a361c451e9d2e6dba427c04af4fd1b3c
scottie-schneider <scott@laclave.co>
2020-12-10 16:45:59 -0500

    working on header pt 2.

--- pages/home.js
+++ pages/home.js
@@ -13,11 +13,47 @@ const MainDiv = styled.div`
     background-size: cover;
     background-image: url("/img/hero.jpg");
     clip-path: polygon(0 0, 100% 0, 100% 75vh, 0% 100%);
+    position: relative;
     .overlay {
       height: 100%;
       width: 100%;
       background-image: linear-gradient(to right bottom, #7ed56f, #28b485);
       opacity: 80%;
+      position: absolute;
+      z-index: -10;
+    }
+    .other {
+    }
+    .logo-box {
+      position: absolute;
+      top: 40px;
+      left: 40px;
+    }
+    .text-box {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .heading-primary {
+      z-index: 5;
+      color: #fff;
+      text-transform: uppercase;
+    }
+    .heading-primary-main {
+      display: block;
+      font-size: 60px;
+      font-weight: 400;
+      letter-spacing: 35px;
+    }
+    .heading-primary-sub {
+      display: block;
+      font-size: 20px;
+      font-weight: 700;
+      letter-spacing: 17.4px;
+    }
+    .logo {
+      height: 35px;
     }
   }
 `;
@@ -50,7 +86,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="header">
-        <div className="overlay">Some text...</div>
+        <div className="overlay"></div>
+        <div className="other">
+          <div className="logo-box">
+            <img
+              src="/img/logo-white.png"
+              alt="Natours logo"
+              className="logo"
+            />
+          </div>
+          <div className="text-box">
+            <h1 className="heading-primary">
+              <span className="heading-primary-main">Outdoors </span>
+              <span className="heading-primary-sub">is where life happens</span>
+            </h1>
+          </div>
+        </div>
       </header>
     </MainDiv>
   );
