a99259bdb7898a669ab3725fe7f3c14b1f5124b7
Scottie Schneider <scott@laclave.co>
2020-12-13 09:48:10 -0500

    buttons and transforms

--- pages/home.js
+++ pages/home.js
@@ -29,13 +29,45 @@ const theme = {
 	white: '#fff',
 	black: '#000',
 	grey_light_1: '#f7f7f7',
+	// Font
+	default_font: '1.6rem',
 };
 const StyledMain = styled.main`
+	/* Animations */
+
+	/* Base */
+	.tour-marketing {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+	.btn-text {
+		&:link,
+		&:visited {
+			font-size: ${props => props.theme.default_font};
+			color: ${props => props.theme.primary};
+			display: inline-block;
+			text-decoration: none;
+			border-bottom: 1px solid ${props => props.theme.primary};
+			padding: 3px;
+			transition: all 0.2s;
+		}
+		&:hover {
+			background-color: ${props => props.theme.primary};
+			color: ${props => props.theme.white};
+			box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
+			transform: translateY(-2px);
+		}
+		&:active {
+			box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
+			transform: translateY(0);
+		}
+	}
 	.section-about {
 		background-color: ${props => props.theme.grey_light_1};
 		padding: 25rem 0;
 		margin-top: -20vh;
 	}
+	/* Typography */
 	.heading-secondary {
 		font-size: 3.5rem;
 		text-transform: uppercase;
@@ -53,152 +85,41 @@ const StyledMain = styled.main`
 		-webkit-background-clip: text;
 		/* 4 - text goes transparent to allow bg to show */
 		color: transparent;
+		letter-spacing: 2px;
+		transition: all 0.2s;
+		&:hover {
+			transform: skewY(2deg);
+			text-shadow: 0.5rem 1rem 2rem rgba(0, 0, 0);
+		}
+	}
+	.heading-tertiary {
+		font-size: ${props => props.theme.default_font};
+		font-weight: 700;
+		text-transform: uppercase;
+	}
+	.paragraph {
+		font-size: 1.5rem;
+		&:not(:last-child) {
+			margin-bottom: 3rem;
+		}
+	}
+	/* Utilities */
+	.u-center-text {
+		text-align: center;
+	}
+	.u-margin-bottom-big {
+		margin-bottom: 8rem;
+	}
+	.u-margin-bottom-medium {
+		margin-bottom: 6rem;
+	}
+	.u-margin-bottom-small {
+		margin-bottom: 2rem;
 	}
 `;
 const MainDiv = styled.div`
 	/* Basic reset */
 	min-height: 100vh;
-	.header {
-		height: 95vh;
-		background-size: cover;
-		background-image: url('/img/hero.jpg');
-		clip-path: polygon(0 0, 100% 0, 100% 75vh, 0% 100%);
-		position: relative;
-		.overlay {
-			height: 100%;
-			width: 100%;
-			background-image: linear-gradient(to right bottom, #7ed56f, #28b485);
-			opacity: 80%;
-			position: absolute;
-			z-index: -10;
-		}
-		.other {
-		}
-		.header__logo-box {
-			position: absolute;
-			top: 40px;
-			left: 40px;
-		}
-		.header__text-box {
-			position: absolute;
-			top: 40%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			text-align: center;
-		}
-		.header__logo {
-			height: 35px;
-		}
-		.heading-primary {
-			z-index: 5;
-			color: #fff;
-			text-transform: uppercase;
-			/* Animation shakiness fix */
-			backface-visibility: hidden;
-			margin-bottom: 60px;
-		}
-		.heading-primary-main {
-			display: block;
-			font-size: 60px;
-			font-weight: 400;
-			letter-spacing: 35px;
-			/* Animations */
-			animation-name: moveInLeft;
-			animation-duration: 1s;
-			animation-timing-function: ease-out;
-		}
-		.heading-primary-sub {
-			display: block;
-			font-size: 20px;
-			font-weight: 700;
-			letter-spacing: 17.4px;
-			/* Animations */
-			animation: moveInRight 1s ease-out;
-		}
-
-		/* Buttons */
-		.btn:link,
-		.btn:visited {
-			display: inline-block;
-			text-transform: uppercase;
-			text-decoration: none;
-			padding: 15px 40px;
-			border-radius: 100px;
-			transition: all 0.2s;
-			position: relative;
-		}
-		.btn:hover {
-			transform: translateY(-3px);
-			box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-		}
-		.btn:active {
-			transform: translateY(-1px);
-			box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-		}
-		.btn-white {
-			background-color: #fff;
-			color: #777;
-		}
-		.btn::after {
-			content: '';
-			display: inline-block;
-			height: 100%;
-			width: 100%;
-			border-radius: 100px;
-			position: absolute;
-			top: 0;
-			left: 0;
-			z-index: -1;
-			transition: all 0.4s;
-		}
-		.btn-white::after {
-			background-color: #fff;
-		}
-		.btn:hover::after {
-			transform: scaleX(1.4) scaleY(1.6);
-			opacity: 0;
-		}
-		.btn-animated {
-			animation: moveInBottom 0.5s ease-out 0.75s;
-			animation-fill-mode: backwards;
-		}
-		@keyframes moveInLeft {
-			0% {
-				opacity: 0;
-				transform: translateX(-100px);
-			}
-			80% {
-				transform: translateX(10px);
-			}
-			100% {
-				opacity: 1;
-				transform: translate(0);
-			}
-		}
-		@keyframes moveInRight {
-			0% {
-				opacity: 0;
-				transform: translateX(100px);
-			}
-			80% {
-				transform: translateX(-10px);
-			}
-			100% {
-				opacity: 1;
-				transform: translate(0);
-			}
-		}
-		@keyframes moveInBottom {
-			0% {
-				opacity: 0;
-				transform: translateY(30px);
-			}
-			100% {
-				opacity: 1;
-				transform: translate(0);
-			}
-		}
-	}
 `;
 
 export default function Home() {
@@ -248,7 +169,34 @@ export default function Home() {
 			</StyledHeader>
 			<StyledMain theme={theme}>
 				<section class="section-about">
-					<h2 class="heading-secondary">Exciting tours for adventurous people</h2>
+					<div class="u-center-text u-margin-bottom-big">
+						<h2 class="heading-secondary">Exciting tours for adventurous people</h2>
+					</div>
+					<div class="tour-marketing">
+						<div class="text">
+							<h3 class="heading-tertiary u-margin-bottom-small">
+								You're going to fall in love with nature
+							</h3>
+							<p class="paragraph">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit
+								similique repellat inventore voluptatum maxime, non accusantium, ea reiciendis
+								atque tempora beatae perspiciatis, temporibus ipsam repudiandae blanditiis
+								ipsa error qui illum?
+							</p>
+							<h3 class="heading-tertiary u-margin-bottom-small">
+								Live adventurous as you never have before
+							</h3>
+							<p class="paragraph">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nulla quasi
+								cum sunt totam iusto dolore veritatis officia quos repellendus quia excepturi,
+								in porro adipisci aperiam, commodi quidem iste perferendis.
+							</p>
+							<a href="#" class="btn-text">
+								Learn more {String.fromCharCode(8594)}
+							</a>
+						</div>
+						<div class="images">Image composition</div>
+					</div>
 				</section>
 			</StyledMain>
 		</MainDiv>
